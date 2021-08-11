570b5bd81d64877cf8d55c38a669be9643da909c
Ivan <abr-iv13@yandex.ru>
2021-08-11 20:10:38 +0300

    update

--- Level-2/patterns/facade/main.go
+++ Level-2/patterns/facade/main.go
@@ -1,23 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	patternFacade "github.com/abr-iv13/WB-Tech/tree/master/Level-2/patterns/facade/pattern-facade"
 )
 
 func main() {
-	fmt.Println()
+	//Инициализация экземплярa класса.
 	walletFacade := patternFacade.NewWalletFacade("abc", 1234)
-	fmt.Println()
 
+	//Добавление средств в кошелек
 	err := walletFacade.AddMoneyToWallet("abc", 1234, 10)
 	if err != nil {
 		log.Fatalf("Error: %s\n", err.Error())
 	}
 
-	fmt.Println()
+	//Списание средств с кошелька.
 	err = walletFacade.DeductMoneyFromWallet("abc", 1234, 5)
 	if err != nil {
 		log.Fatalf("Error: %s\n", err.Error())
