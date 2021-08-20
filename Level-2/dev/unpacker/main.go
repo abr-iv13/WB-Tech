19681eab141d5f0903d7c96a29fd06b2c6dddee7
Ivan <abr-iv13@yandex.ru>
2021-08-20 17:52:30 +0300

    update unpack

--- Level-2/dev/unpacker/main.go
+++ Level-2/dev/unpacker/main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/abr-iv13/WB-Tech/tree/master/Level-2/dev/unpacker/pgk/unpack"
+)
+
 func main() {
+	r1, _ := unpack.Unpack("a4bc2d5e")
+	r2, _ := unpack.Unpack("abcd")
+	r3, _ := unpack.Unpack("45")
+	r4, _ := unpack.Unpack("")
+
+	fmt.Println(r1)
+	fmt.Println(r2)
+	fmt.Println(r3)
+	fmt.Println(r4)
+
 }
--- Level-2/dev/unpacker/pgk/unpack/unpack.go
+++ Level-2/dev/unpacker/pgk/unpack/unpack.go
@@ -7,27 +7,50 @@ import (
 	"unicode"
 )
 
-func Unpack(s string) (r string, err error) {
+//Преобразование string в rune, проверка строки на наличии пробелов
+func isEscape(char rune, prev rune) bool {
+	return string(char) == "\\" && string(prev) != "\\"
+}
+
+//Проверка руны на десятичное число и на наличие пробелов
+func isDecimalRuneAndEscaped(char rune, escaped bool) bool {
+	//IsDigit проверяет является ли руна десятичной цифрой
+	return unicode.IsDigit(char) && !escaped
+}
+
+func Unpack(s string) (string, error) {
+	// Проверка на пустую строку
+	if len(s) == 0 {
+		return "", nil
+	}
+	//Преобразование строки в int
 	if _, err := strconv.Atoi(s); err == nil {
-		return r, errors.New("некорректная строка")
+		return "", errors.New("Некорректная строка")
 	}
 
-	var prev rune
-	var escaped bool
-	var b strings.Builder
+	var prev rune         //Предыдущее значение
+	var escaped bool      //flag на наличие пробелов
+	var b strings.Builder //Построитель используется для эффективного построения строки с использованием методов записи.
+
 	for _, char := range s {
-		if unicode.IsDigit(char) && !escaped {
-			m := int(char - '0')
-			r := strings.Repeat(string(prev), m-1)
+		if isDecimalRuneAndEscaped(char, escaped) {
+			//Преобразование руны в число
+			//«вычитание значения руны« 0 »из любой руны от« 0 »до« 9 »даст целое число от 0 до 9».
+			i := int(char - '0')
+			//Repeat возвращает новую строку, состоящую из количества копий строки.
+			r := strings.Repeat(string(prev), i-1)
+			//Добавление строку в буфер
 			b.WriteString(r)
 		} else {
-			escaped = string(char) == "\\" && string(prev) != "\\"
+			escaped = isEscape(char, prev)
 			if !escaped {
+				//Добавление rune в буфер
 				b.WriteRune(char)
 			}
+
 			prev = char
 		}
 	}
 
-	return b.String(), err
+	return b.String(), nil
 }
