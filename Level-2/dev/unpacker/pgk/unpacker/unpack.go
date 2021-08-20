0b93b9b9af4a01e4bddc4218c7a5c717d9e06fcd
Ivan <abr-iv13@yandex.ru>
2021-08-20 10:07:03 +0300

    update unpack

--- Level-2/dev/unpacker/pgk/unpacker/unpack.go
+++ Level-2/dev/unpacker/pgk/unpacker/unpack.go
@@ -1,1 +1,33 @@
 package unpacker
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func Unpack(s string) (r string, err error) {
+	if _, err := strconv.Atoi(s); err == nil {
+		return r, errors.New("некорректная строка")
+	}
+
+	var prev rune
+	var escaped bool
+	var b strings.Builder
+	for _, char := range s {
+		if unicode.IsDigit(char) && !escaped {
+			m := int(char - '0')
+			r := strings.Repeat(string(prev), m-1)
+			b.WriteString(r)
+		} else {
+			escaped = string(char) == "\\" && string(prev) != "\\"
+			if !escaped {
+				b.WriteRune(char)
+			}
+			prev = char
+		}
+	}
+
+	return b.String(), err
+}
