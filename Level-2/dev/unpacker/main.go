20a4ed4d917d837b61195bfcf26f20f91f1ead47
Ivan <abr-iv13@yandex.ru>
2021-08-20 10:16:40 +0300

    update unpack

--- Level-2/dev/unpacker/main.go
+++ Level-2/dev/unpacker/main.go
@@ -1,6 +1,9 @@
 package main
 
+import (
+	"github.com/abr-iv13/WB-Tech/tree/master/Level-2/dev/unpacker/pkg/unpack"
+)
+
 func main() {
-	str := "aaaabccddddde"
-	result := Unpack()
+	unpack.Unpack("ss")
 }
--- Level-2/dev/unpacker/pgk/unpack/unpack.go
+++ Level-2/dev/unpacker/pgk/unpack/unpack.go
@@ -1,4 +1,4 @@
-package unpacker
+package unpack
 
 import (
 	"errors"
