c7afc30a1716e8ae64866d5501954d899b6a2461
Ivan <abr-iv13@yandex.ru>
2021-08-24 21:45:36 +0300

    update 17

--- Level-1/17/17.go
+++ Level-1/17/17.go
@@ -4,22 +4,24 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
 func checkType(param interface{}) {
-	// reflect.TypeOf(param)
-	switch v := param.(type) {
-	case int:
-		fmt.Println("int")
-	case string:
-		fmt.Println("string")
-	case bool:
-		fmt.Println("bool")
-	case chan int:
-		fmt.Println("chan int")
-	default:
-		fmt.Println("Переменная не определенна", v)
-	}
+	fmt.Println(reflect.TypeOf(param))
+
+	//switch v := param.(type) {
+	//case int:
+	//	fmt.Println("int")
+	//case string:
+	//	fmt.Println("string")
+	//case bool:
+	//	fmt.Println("bool")
+	//case chan int:
+	//	fmt.Println("chan int")
+	//default:
+	//	fmt.Println("Переменная не определенна", v)
+	//}
 }
 
 func main() {
